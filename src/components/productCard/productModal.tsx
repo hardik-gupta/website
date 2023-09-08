87fc624ef2955ee57ed7b27af0b334873326b740
Shruti3004 <mail2shruti.ag@gmail.com>
2023-09-08 19:06:42 +0530

    feat: contributor experience
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- src/components/productCard/productModal.tsx
+++ src/components/productCard/productModal.tsx
@@ -94,7 +94,7 @@ export const ProductModal: React.FC<ProjectProps> = ({ projects }) => {
                   <img
                     src="/video.png"
                     alt="GitHub Logo"
-                    className="max-w-[100px]"
+                    className="max-w-[80px]"
                   />
                 </div>
                 <a
