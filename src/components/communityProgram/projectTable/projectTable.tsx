9f75f32c728f0f8707a17bc25e87af26cab700fd
Hardik Gupta <hardik.gupta1127@gmail.com>
2024-02-04 12:44:00 +0530

    Pulled changes from Upstream Main
    pull from upstream main

--- src/components/communityProgram/projectTable/projectTable.tsx
+++ src/components/communityProgram/projectTable/projectTable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import * as React from "react"
+import React, {useState} from "react"
 
 import {
   useReactTable,
@@ -27,20 +27,23 @@ import { DataTablePagination } from "@/component/components/communityProgram/pro
 import { DataTableViewOptions } from "@/component/components/communityProgram/projectTable/columnVisibility";
 import { ColumnFilter } from "@/component/components/communityProgram/projectTable/columnFilter";
 import { AppliedFilters } from "@/component/components/communityProgram/projectTable/appliedFilters";
+import { ProjectDetails } from "./columns";
 
 interface ProjectTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  filterOptions: ProjectDetails[];
 }
 
 export const ProjectTable = <TData, TValue>({
   columns,
   data,
+  filterOptions
 }: ProjectTableProps<TData, TValue>) => {
-  const [sorting, setSorting] = React.useState<SortingState>([])
-  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
-  const [globalFilter, setGlobalFilter] = React.useState<any>("")
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+  const [sorting, setSorting] = useState<SortingState>([{ id: 'status', desc: false }]); // inner object for default sorting to 'status'
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [globalFilter, setGlobalFilter] = useState<any>("");
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   const table = useReactTable({
     data,
@@ -60,48 +63,66 @@ export const ProjectTable = <TData, TValue>({
       globalFilter,
     },
   });
+
+  const tableTechSkills = [...new Set(filterOptions.flatMap(tableItem => tableItem.reqd_skills).filter(item => item != null))];
+  const tableProducts = [...new Set(filterOptions.flatMap(tableItem => tableItem.product).filter(item => item != null))];
+  const tableOrganization = [...new Set(filterOptions.flatMap(tableItem => tableItem.organization).filter(item => item != ""))];
+  let tableProjectTypes = [...new Set(filterOptions.flatMap(tableItem => tableItem.project_category).filter(type => type != null))];
+  tableProjectTypes = [...tableProjectTypes, ...new Set(filterOptions.flatMap(tableItem => tableItem.project_sub_category).filter(subType => subType != null))];
+
+  console.log(table)
   return (
     <div className="flex flex-col text-black">
       <div className="table-filters w-full p-2 flex flex-col-reverse lg:flex-row">
+        
         <div className="applied-filters w-full lg:w-1/3 flex flex-wrap items-center justify-start">
-          <AppliedFilters table={table} columnFilters={columnFilters} setColumnFilters={setColumnFilters}/>
+          <AppliedFilters table={table} columnFilters={columnFilters} setColumnFilters={setColumnFilters} />
         </div>
+
+       
         <div className="filter-options lg:w-2/3 flex flex-wrap items-center justify-start lg:justify-end">
           {table.getColumn("complexity") && (
             <ColumnFilter
-            column={table.getColumn("complexity")}
-            title="complexity"
-            options={["low", "medium", "high"]}
+              column={table.getColumn("complexity")}
+              title="complexity"
+              options={["Low", "Medium", "High"]}
             />
           )}
           {table.getColumn("techSkills") && (
             <ColumnFilter
-            column={table.getColumn("techSkills")}
-            title="tech skills"
-            options={["Github Actions", "Java"]}
+              column={table.getColumn("techSkills")}
+              title="tech skills"
+              options={tableTechSkills}
             />
-            )}
+          )}
           {table.getColumn("points") && (
             <ColumnFilter
               column={table.getColumn("points")}
               title="points"
-              options={["10", "20", "30"]}
+              options={[10, 20, 30]}
             />
           )}
-          {table.getColumn("product") && (
+          {/* {table.getColumn("product") && (
             <ColumnFilter
-            column={table.getColumn("product")}
-            title="Product"
-            options={["CORD Network", "Sunbird UCI"]}
+              column={table.getColumn("product")}
+              title="Product"
+              options={tableProducts}
+            />
+          )} */}
+          {table.getColumn("organization") && (
+            <ColumnFilter
+              column={table.getColumn("organization")}
+              title="Organization"
+              options={tableOrganization}
             />
           )}
-          {table.getColumn("type") && (
+          {/* {table.getColumn("type") && (
             <ColumnFilter
-            column={table.getColumn("type")}
-            title="Project Type"
-            options={["DevOps", "NextJS", "Github Actions"]}
+              column={table.getColumn("type")}
+              title="Project Type"
+              options={tableProjectTypes}
             />
-            )}
+          )} */}
           {table.getColumn("status") && (
             <ColumnFilter
               column={table.getColumn("status")}
@@ -110,23 +131,32 @@ export const ProjectTable = <TData, TValue>({
             />
           )}
         </div>
+
       </div>
 
+      
+
       <div className="table-options flex flex-col lg:flex-row items-center justify-between p-2 lg:p-3 w-full">
+        
         <div className="flex items-center space-x-1 w-full mb-2">
           <input
-              className="px-2 h-9 w-full lg:w-[400px] border-2 border-gray-200 rounded-md justify-between"
-              placeholder="Filter By Project Name..."
-              value={globalFilter}
-              onChange={(event) =>setGlobalFilter(event.target.value)}
+            className="px-2 h-9 w-full lg:w-[400px] border-2 border-gray-200 rounded-md justify-between"
+            placeholder="Filter By Project Name..."
+            value={globalFilter}
+            onChange={(event) => setGlobalFilter(event.target.value)}
           />
-          <DataTableViewOptions table={table}/>
+          <DataTableViewOptions table={table} />
         </div>
+
+
         <div className="pagination flex items-center">
-          <DataTablePagination table={table}/>
+          <DataTablePagination table={table} />
         </div>
+        
       </div>
-      
+      <div className="w-full items-center justify-start text-black text-right pr-4 border-b border-[#f4f4f] pb-2">
+         {JSON.stringify(table.getFilteredRowModel().flatRows.length)} Total tickets
+        </div>
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
@@ -137,9 +167,9 @@ export const ProjectTable = <TData, TValue>({
                     {header.isPlaceholder
                       ? null
                       : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
                   </TableHead>
                 );
               })}
